d68f367d3713c3ea806c56cd4bdc825aa9c9d30f
Rylan Kauffman <rjk94@pitt.edu>
2025-07-14 16:50:41 -0400

    Progress on workout display and grouping (frontend)

--- frontend/src/components/activeWorkout/ExerciseCards.tsx
+++ frontend/src/components/activeWorkout/ExerciseCards.tsx
@@ -1,19 +1,20 @@
 import { Workout } from '@/store/workout'
-import { Container, VStack, HStack, Text, For, Card, Heading } from '@chakra-ui/react'
-import React from 'react'
+import { Container, VStack, HStack, Text, For, CheckboxCard, Heading, Flex } from '@chakra-ui/react'
+import React, { useEffect, useState } from 'react'
+import { RxCross2 } from "react-icons/rx";
 
-const ExerciseCard = ({workout} : {workout : Workout | undefined}) => {
+const ExerciseCard = ({workout, select, setCount} : {workout : Workout | undefined, select : boolean, setCount : React.Dispatch<React.SetStateAction<number>>}) => {
   if (workout === undefined) {
     return (<Text>Loading...</Text>)
   }
 
   return (
-    <Container h={"100vh"} w={"100vw"}>
-      <VStack h={"100%"} w={"100%"} gap={4} padding={4}>
+    <Container w="100vw">
+      <VStack gap={4} padding={4}>
         {console.log("ExerciseCard rendered with workout:", workout)}
         <For each={workout.exercises} fallback={<Text>No exercises found</Text>}>
           {(exercise) => (
-            <SingleExerciseCard exercise={exercise} />
+            <SingleExerciseCard exercise={exercise} select={select} setCount={setCount} />
           )}
         </For>
       </VStack>
@@ -21,25 +22,51 @@ const ExerciseCard = ({workout} : {workout : Workout | undefined}) => {
   )
 }
 
-const SingleExerciseCard = ({exercise} : {exercise: Workout['exercises'][number]}) => {
+// TODO: Pull checked state to parent component
+const SingleExerciseCard = ({exercise, select, setCount} : {exercise: Workout['exercises'][number], select : boolean, setCount : React.Dispatch<React.SetStateAction<number>>}) => {
+  const [checked, setChecked] = useState(false)
+
+  useEffect(() => {
+    if (select === false) {
+      setChecked(false)
+      setCount(0)
+    }
+  }, [select, setCount])
+
   return (
-    <Card.Root size="md">
-      <Card.Header>
-        <Heading size="lg">{exercise.exerciseId}</Heading>
-      </Card.Header>
-      <Card.Body color="fg.muted">
-        <HStack w={"full"}>
-          <For each={exercise.set_data}>
-          {(set) => (
+    <CheckboxCard.Root checked={checked} variant={"subtle"} w={"20vw"} onChange={(e) => {
+      console.log(e)
+      if (select) {
+        if (checked === false) {
+          setCount(prevCount => prevCount + 1)
+        } else if (checked === true) {
+          setCount(prevCount => prevCount - 1)
+        }
+        setChecked(prevCheck => !prevCheck)
+      }
+      }}>
+      <CheckboxCard.HiddenInput />
+      <CheckboxCard.Control>
+        <CheckboxCard.Label>
+          <Flex w={"full"} direction={"row"} justify="space-between">
+            <Heading size="lg">{exercise.exerciseId}</Heading>
             <HStack>
-              <Text>{set.weight} weight</Text>
-              <Text>{set.reps} reps</Text>
+              <Text>3</Text>
+              <Text>X</Text>
+              <Text>10</Text>
+              {/* <For each={exercise.set_data}>
+              {(set) => (
+                <HStack>
+                <Text>{set.weight} weight</Text>
+                <Text>{set.reps} reps</Text>
+                </HStack>
+                )}
+                </For> */}
             </HStack>
-          )}
-          </For>
-        </HStack>
-      </Card.Body>
-    </Card.Root>
+          </Flex>
+        </CheckboxCard.Label>
+      </CheckboxCard.Control>
+    </CheckboxCard.Root>
   )
 }
 
--- frontend/src/pages/ActiveWorkout.tsx
+++ frontend/src/pages/ActiveWorkout.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
-import { Box, CloseButton, Container, Flex, VStack } from '@chakra-ui/react'
+import { Box, CloseButton, Container, Flex, Button, Center, ActionBar, Portal } from '@chakra-ui/react'
 import { useWorkoutStore, Workout } from '@/store/workout';
 import { useUserStore } from '@/store/user';
 import ExerciseCards from '@/components/activeWorkout/ExerciseCards';
+import { FaLayerGroup } from "react-icons/fa";
 
 const ActiveWorkout = ({funcNav} : {funcNav: React.Dispatch<React.SetStateAction<boolean>>}) => {
   const navigate = useNavigate();
@@ -25,16 +26,51 @@ const ActiveWorkout = ({funcNav} : {funcNav: React.Dispatch<React.SetStateAction
     navigate(-1); // navigate to workouts page
   }
 
+  // Checks if the select button has been clicked or not
+  const [checked, setChecked] = useState(false)
+
+  // Keep track of number of exercises selected
+  const [count, setCount] = useState(0)
+
   return (
-    <Container h={"100vh"} w={"100vw"} >
-    {console.log("ActiveWorkout rendered with workoutId:", workoutId)}
-      <Flex direction={'column'} justify={'flex-start'}>
-        <Box w={"100%"} display={"flex"} justifyContent={"flex-start"} padding={4}>
-          <CloseButton variant="solid" onClick={handleClick} />
-        </Box>
-        <ExerciseCards workout={workout} />
-      </Flex>
-    </Container>
+    <>
+      <Container h={"100vh"} w={"100vw"} justifyContent={"space-between"}>
+        <Center>
+          <Flex direction={'column'} justify={'flex-start'} maxH={"90vh"}>
+            <Box w={"100%"} display={"flex"} justifyContent={"flex-start"} padding={4}>
+              <CloseButton variant="solid" onClick={handleClick} />
+            </Box>
+            <ExerciseCards workout={workout} select={checked} setCount={setCount} />
+          </Flex>
+        </Center>
+        <Center>
+          <Flex justify={"space-between"} w={"20vw"}>
+            <Button variant="subtle" colorPalette="green" px={"10"} py={"5"} onClick={() => setChecked(prevCheck => !prevCheck)} >Select</Button>
+            <Button colorPalette="green" px={"10"} py={"5"}>Start</Button>
+            {/* <Button variant="subtle" disabled colorPalette="green" px={"10"} py={"5"}>Group</Button> */}
+          </Flex>
+        </Center>
+      </Container>
+      <ActionBar.Root open={checked} onOpenChange={(e) => setChecked(e.open)} closeOnInteractOutside={false}>
+        <Portal>
+          <ActionBar.Positioner>
+            <ActionBar.Content>
+              <ActionBar.SelectionTrigger>
+                {count} selected
+              </ActionBar.SelectionTrigger>
+              <ActionBar.Separator />
+              <Button variant="outline" size="sm" onClick={() => console.log("hello")}>
+                <FaLayerGroup />
+                Group
+              </Button>
+              <ActionBar.CloseTrigger asChild>
+                <CloseButton size="sm" />
+              </ActionBar.CloseTrigger>
+            </ActionBar.Content>
+          </ActionBar.Positioner>
+        </Portal>
+      </ActionBar.Root>
+    </>
   )
 }
 
